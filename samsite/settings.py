ea79fdc5fcf978ead3f84278f33d22a7ddcef04c
SamanthaBadeau <samantha.badeau@gmail.com>
2021-06-02 14:48:56 -0400

    changes6

--- samsite/settings.py
+++ samsite/settings.py
@@ -17,6 +17,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+if os.path.exists('env.py'):
+    import env
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
